42e6c1aa7bcbfa6951b5a319069b2d81ff1192f1
Valentin DELAPRE <valentin.delapre@gmail.com>
2018-01-04 16:58:13 +0100

    presque fin

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
   <meta charset="utf-8">
-  <link rel="stylesheet" href="/css/style_prive.css">
+  <link rel="stylesheet" href="assets/css/style_prive.css">
   <title></title>
 </head>
 <body>
---
+++ reference.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <meta charset="utf-8">
+  <link href="assets/css/style_partenaire.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+  <header>
+    <?php
+    include("navbar.php")
+    ?>
+  </header>
+  <div class="container" style="margin-top:100px;">
+    <div class="row" id="div_logo">
+      <img class="img-responsive col-xs-12 col-sm-2" src="assets/img/logos/200px-Ministe%CC%80re_de_l_Economie_et_des_Finances_(France).png"/>
+      <img class="img-responsive col-xs-12 col-sm-2" src="assets/img/logos/350px-Orange_logo.svg.png"/>
+      <img class="img-responsive col-xs-12 col-sm-2" src="assets/img/logos/9036f182df34aa4f54e5d0152206d269.png"/>
+      <img class="img-responsive col-xs-12 col-sm-2" src="assets/img/logos/ALTRAN_RGB.jpg"/>
+      <img class="img-responsive col-xs-12 col-sm-2" src="assets/img/logos/axa.jpg"/>
+      <img class="img-responsive col-xs-12 col-sm-2" src="assets/img/logos/edf-logo-rgb-300-f.jpg"/>
+    </div>
+    <div class="row" id="div_logo">
+      <img class="img-responsive col-xs-6 col-sm-2" src="assets/img/logos/Havas_logo-png.png"/>
+      <img class="img-responsive col-xs-6 col-sm-2" src="assets/img/logos/LogoAdetem.png"/>
+      <img class="img-responsive col-xs-6 col-sm-2" src="assets/img/logos/logoM3.png"/>
+      <img class="img-responsive col-xs-6 col-sm-2" src="assets/img/logos/MAIRIE13_LOGO.png"/>
+      <img class="img-responsive col-xs-6 col-sm-2" src="assets/img/logos/novigere.jpg"/>
+      <img class="img-responsive col-xs-6 col-sm-2" src="assets/img/logos/EIT-ICT-Labs-Logo.jpg"/>
+    </div>
+  </div>
+  <footer>
+    <?php
+    include("footer.php")
+    ?>
+  </footer>
+</body>
+
+</html>
