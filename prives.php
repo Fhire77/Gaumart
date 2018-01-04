8b05932b63007963034f68ef1849d66c90e421cf
Alexis Faucher <34653602+AlexFau16@users.noreply.github.com>
2018-01-04 12:46:20 +0100

    ske jé fé avan github

---
+++ prives.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width; initial-scale=1.0">
+<link href="bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+<link href="style.css" type="text/css" rel="stylesheet">
+
+<title>GAUMART Traiteur Paris</title>
+</head>
+
+<body>
+<header>
+ <nav class="navbar navbar-default" role="navigation">
+    	  <div class="container">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-brand-centered">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <div class="navbar-brand navbar-brand-centered"><div class="col-xs-12"><img id="logo" src="img/logo_gaumart.png"></div></div></div>
+
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="navbar-brand-centered">
+		      <ul class="nav navbar-nav">
+		        <li><a href="#">Réceptions d'entreprise</a></li>
+		        <li><a href="#">Evénements privés</a></li>
+		      </ul>
+		      <ul class="nav navbar-nav navbar-right">
+		        <li><a href="#">Traiteur</a></li>
+		        <li><a href="#">Nos références</a></li>
+		        <li><a href="#">Nous contacter</a></li>		        
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+</header>
+
+<div class="hidden-xs" id="trait1"></div>
+<div class="hidden-xs" id="trait2"></div>
+
+<div id="conteneur">
+    <div class="flex1">
+        <center><h3><b>Evénements privés</b></h3>
+            <p id="textedegauche">Organisation de votre dîner haut de gamme à Paris<br>
+            Pompeius iuris ludaers provinciae calentes aptae<br>
+            medelarum navigerum nusquam multiplicium navigerum<br>
+            plurimis navigerum sorte delata et plurimis navigerum<br>
+            ludaers in</p></center>
+
+        <div class="image1"><center><img class="img-responsive" src="img/csm_traiteur_creatif_731aa686b3.jpg" width="75%" height="75%"></center></div>
+    </div>
+
+    <div class="flex2">
+        <div class="image2"><center><img class="img-responsive" src="img/sale_sucre.jpg" width="100%" height="100%"></center></div>
+    </div>
+    
+    <div class="flex3">
+        <div class="image3"><center><img class="img-responsive" src="img/csm_traiteur_patissier_9daec18485.jpg" width="75%" height="75%"></center>
+        </div>
+        <div class="image4"><center><img class="img-responsive" src="img/csm_patissier_traiteur_a_paris_2d2c89ae22.jpg" width="75%" height="75%"></center>
+        </div>
+    </div>
+</div>
+
+<div id="footer">
+    <center><div id="ligne1"><b>- GAUMART Traiteur Paris -</b></div></center>
+    <center><div id="ligne2">Mentions légales || Gateartproject  2015</div></center>
+</div>
+</body>
+<script src="bootstrap/js/jquery-3.2.1.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</html>
---
+++ references.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width; initial-scale=1.0">
+<link href="bootstrap/css/bootstrap.min.css" type="text/css" rel="stylesheet">
+<link href="style.css" type="text/css" rel="stylesheet">
+
+<title>GAUMART Traiteur Paris</title>
+</head>
+
+<body>
+<header>
+ <nav class="navbar navbar-default" role="navigation">
+    	  <div class="container">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-brand-centered">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <div class="navbar-brand navbar-brand-centered"><div class="col-xs-12"><img id="logo" src="img/logo_gaumart.png"></div></div></div>
+
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="navbar-brand-centered">
+		      <ul class="nav navbar-nav">
+		        <li><a href="#">Réceptions d'entreprise</a></li>
+		        <li><a href="#">Evénements privés</a></li>
+		      </ul>
+		      <ul class="nav navbar-nav navbar-right">
+		        <li><a href="#">Traiteur</a></li>
+		        <li><a href="#">Nos références</a></li>
+		        <li><a href="#">Nous contacter</a></li>		        
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+</header>
+
+<div class="hidden-xs" id="trait1"></div>
+<div class="hidden-xs" id="trait2"></div>
+
+<div id="conteneur2">
+    <div class="flex4">
+       
+        <div class="image5"><center><img class="img-responsive" src="img/logos/350px-Orange_logo.svg.png" width="100%" height="100%"></center></div>
+        
+        <div class="image6"><center><img class="img-responsive" src="img/logos/EIT-ICT-Labs-Logo.jpg" width="100%" height="100%"></center></div>
+        
+        <div class="image7"><center><img class="img-responsive" src="img/logos/9036f182df34aa4f54e5d0152206d269.png" width="100%" height="100%"></center></div>
+        
+        <div class="image8"><center><img class="img-responsive" src="img/logos/200px-Ministe%CC%80re_de_l_Economie_et_des_Finances_(France).png" width="100%" height="100%"></center></div>
+        
+        <div class="image9"><center><img class="img-responsive" src="img/logos/ministere_economie2.png" width="100%" height="100%"></center></div>
+        
+    </div>
+    
+    <div class="flex5">
+       
+        <div class="image10"><center><img class="img-responsive" src="img/logos/MAIRIE13_LOGO.png" width="100%" height="100%"></center>
+        </div>
+        
+        <div class="image11"><center><img class="img-responsive" src="img/logos/LogoAdetem.png" width="100%" height="100%"></center>
+        </div>
+        
+        <div class="image12"><center><img class="img-responsive" src="img/logos/edf-logo-rgb-300-f.jpg" width="100%" height="100%"></center>
+        </div>
+        
+        <div class="image13"><center><img class="img-responsive" src="img/logos/Havas_logo-png.png" width="100%" height="100%"></center>
+        </div>
+    </div>
+    
+    <div class="flex6">
+        <div class="image14"><center><img class="img-responsive" src="img/logos/axa.jpg" width="100%" height="100%"></center>
+        </div>
+        
+        <div class="image15"><center><img class="img-responsive" src="img/logos/ALTRAN_RGB.jpg" width="100%" height="100%"></center>
+        </div>
+        
+        <div class="image16"><center><img class="img-responsive" src="img/logos/novigere.jpg" width="100%" height="100%"></center>
+        </div>
+        
+        <div class="image17"><center><img class="img-responsive" src="img/logos/logoM3.png" width="100%" height="100%"></center>
+        </div>
+    </div>
+</div>
+
+
+<div id="footer">
+    <center><div id="ligne1"><b>- GAUMART Traiteur Paris -</b></div></center>
+    <center><div id="ligne2">Mentions légales || Gateartproject  2015</div></center>
+</div>
+</body>
+<script src="bootstrap/js/jquery-3.2.1.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+</html>
