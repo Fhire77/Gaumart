ea20fc3e05329b59e77a1f023d51871d166bad94
Alexis Faucher <34653602+AlexFau16@users.noreply.github.com>
2018-01-04 15:54:35 +0100

    navbar jasu

---
+++ navbar.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>navbar</title>
+    <meta charset="utf-8">
+     <meta http-equiv="Content-type" content="text/html;charset=UTF-8" />
+
+
+    <!-- CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/style_navbar.css" />
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" id="pagesheet" />
+    <link rel="stylesheet" type="text/css" href="assets/css/demo.css" /> <!-- CSS pour logo qui descend en mode hamburger -->
+</head>
+
+    <body>
+
+
+    <!--===========================Header=============================-->
+
+        <div class="container-fluid" style="background-color:white;">
+            <nav class="navbar navbar-default" role="navigation">
+              <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-brand-centered">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <div class="navbar-brand navbar-brand-centered"><a href="index.php"><img id="logo" style="width:3em;
+                      " src="assets/img/logo_gaumartblanc.jpg"/></a>
+                  </div>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="navbar-brand-centered">
+                  <ul class="nav navbar-nav">
+                    <li><a href="Reception.php" id="reception">Reception d'entreprise</a></li>
+                    <li><a href="evenement_prives.php">Evenements privés</a></li>
+                  </ul>
+                  <ul class="nav navbar-nav navbar-right">
+                    <li><a href="traiteur.php" id="traiteur">Traiteur</a></li>
+                    <li><a href="reference.php">Nos références</a></li>
+                    <li><a href="contact.php">Nous contacter</a></li>
+                  </ul>
+                </div><!-- /.navbar-collapse -->
+              </div><!-- /.container-fluid -->
+            </nav>
+        </div>
+
+        <div id="blank"></div>
+
+
+
+
+
+
+
+
+
+
+    </body>
+
+</html>
