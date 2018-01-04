ea47dad363e43bf3de30dcc941914c5b7d404e27
Valentin DELAPRE <valentin.delapre@gmail.com>
2018-01-04 12:47:46 +0100

    footer

--- footer.php
+++ footer.php
@@ -2,11 +2,3 @@
     <center><div id="ligne1"><b>- GAUMART Traiteur Paris -</b></div></center>
     <center><div id="ligne2">Mentions légales || Gateartproject  2015</div></center>
 </div>
-
-
-css :
-#footer{
-    margin-top:1%;
-    font-style: italic;
-}
-
