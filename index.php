db117ac10dbcb2ade280200ffaa5f534d6a4a3e3
Valentin DELAPRE <valentin.delapre@gmail.com>
2018-01-04 14:46:08 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title></title>
+</head>
+<body>
+  <header>
+    <?php
+    include("navbar.php")
+    ?>
+  </header>
+  <div class="slider">
+
+  </div>
+  <footer>
+    <?php
+    include("footer.php")
+    ?>
+  </footer>
+</body>
+</html>
