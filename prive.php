d16e8e5970a7f5863e03f8ea8f1bbb20cdd4bf33
Julien Gauducheau <juliengauducheau17@gmail.com>
2018-01-04 14:59:25 +0100

    prive

---
+++ prive.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="UTF-8">
+		<title>Document</title>
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		
+		
+				<link href='http://fonts.googleapis.com/css?family=Economica:700,400italic' rel='stylesheet' type='text/css'>
+
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		
+<br >
+<br>
+
+<h3>Evenements privés</h3>
+    <p>Organisation de votre diner haut de gamme a Paris.
+   Lorem ipsum dolor sit amet, consectetur<p> adipisicing elit. Culpa iste enim, aspernatur nam maiores minus.</p>
+        <p> Minus soluta, officia voluptas!</p><p> Molestiae voluptatem perferendis tenetur.</p><p> Quaerat porro quasi soluta unde laboriosam reiciendis?
+   </p>
+    <div id="img4" class="center-block">
+    <img src="img/csm_traiteur_creatif_731aa686b3.jpg" alt="" class="">	
+</div>
+<div id="img3">
+    <img src="img/sale_sucre.jpg" alt="" class="photo img-responsive" >
+</div>
+<div id="img1">
+    <img src="img/csm_traiteur_patissier_9daec18485.jpg" alt="" class="photo img-responsive">
+</div>
+<div id="img2">
+    <img src="img/csm_patissier_traiteur_a_paris_2d2c89ae22.jpg" alt="" class="photo img-responsive">
+</div>
+
+      
+  
+	
+
+		
+		
+		<script src="bootstrap/js/jquery-3.2.1.js"></script>
+		<script src="bootstrap/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+		<script type="text/javascript" src="js/jquery.cslider.js"></script>
+		<script src="js/modernizr.custom.28468.js"></script>
+		<script src="js/app.js"></script>
+	</body>
+</html>
